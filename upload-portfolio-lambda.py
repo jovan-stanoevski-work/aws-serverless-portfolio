f3f6a3af303d4098b5840aabac2865059e43edb5
jovan-stanoevski-work <jovan.stanoevski@outlook.com>
2020-10-29 00:01:37 +0100

    Add upload modified lambda portolio script and codebuild buildspec YAML

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,20 +1,29 @@
 import boto3
-from botocore.client import Config
 import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
 
-portfolio_bucket = s3.Bucket('jovan.stanoevski.work')
-build_bucket = s3.Bucket('codebuild.jovan.stanoevski.work')
+    location = {
+        "bucketName":'codebuild.jovan.stanoevski.work',
+        "objectKey": 'codebuild-aws-serverless-portfolio.zip'
+        }
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.dowload_fileobj('codebuild-aws-serverless-portfolio.zip', portfolio_zip)
+    
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm,
-          ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    portfolio_bucket = s3.Bucket('jovan.stanoevski.work')
+    build_bucket = s3.Bucket(location["bucketName"])
+
+    portfolio_zip = StringIO.StringIO()
+
+    build_bucket.download_fileobj(location["objectKey"],portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm,
+            # Remember mimetypes guesses using file extension so make sure exists!
+            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
